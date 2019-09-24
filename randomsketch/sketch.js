26db833c45a8688e15f6dfa3b8c91a23dcee82ae
zainiba <47758673+zainiba@users.noreply.github.com>
2019-09-24 16:27:35 -0700

    Added array

--- randomsketch/sketch.js
+++ randomsketch/sketch.js
@@ -6,3 +6,147 @@ function draw() {
   background(220);
   ellipse(200, 200, 200);
 }
+
+var roster = [{
+      firstName: "Zainib",
+      lastName: "Ahmed",
+      color: "Blue",
+      animal: "Giraffe",
+      book: "The Book Thief",
+      movie: "Amelie",
+      game: "none",
+      superHero: "The Hulk",
+      randomFact: "I love to eat olives."
+    }, {
+      firstName: "Camila",
+      lastName: "Avina Garcia",
+      color: "Green",
+      animal: "Dog",
+      book: "Match",
+      movie: "Lady and the Tramp",
+      game: " Guitar Hero",
+      superHero: "Spiderman",
+      randomFact: "I have a twin",
+    }, {
+      firstName: "Hector",
+      lastName: "Flores",
+      color: "Blue",
+      animal: "Lion",
+      book: "Captain Underpants",
+      movie: "Ace Ventura pet detective",
+      game: "League Of Legends",
+      superHero: "Flash",
+      randomFact: "I'm a Libra",
+    }, {
+      firstName: "Eddson",
+      lastName: "Jose",
+      color: "midnight blue",
+      animal: "pig",
+      book: "The Dark Forest",
+      movie: "The Fountain",
+      game: "Halo Reach",
+      superHero: "Superman",
+      randomFact: "I am made up of stardust",
+    }, {
+      firstName: "Lydia",
+      lastName: "Lim",
+      color: "blue",
+      animal: "dog",
+      book: "No",
+      movie: "me before you",
+      game: "No",
+      superHero: "Thor",
+      randomFact: " birds are tetrapods even though they only walk on two legs",
+    }, {
+      firstName: "Chi",
+      lastName: "Nguyen",
+      color: "purple",
+      animal: "bird",
+      book: "Math Curse",
+      movie: "Big Hero 6",
+      game: "Checkers",
+      superHero: "Big Hero 6 (team)",
+      randomFact: "Lately this year, monsters have especially",
+    }, {
+      firstName: "Leon",
+      lastName: "Quach",
+      color: "silver",
+      animal: "penguins",
+      book: "Letters to a Young Poet",
+      movie: "Kung fu Hustle",
+      game: "Pokemon Crystal Version",
+      superHero: "Batman",
+      randomFact: "I have two pet guinea pigs"
+    }, {
+
+      firstName: "Hao",
+      lastName: "Rong",
+      color: hex(#4285F4),
+      animal: "otter",
+      book: "A Brief History of Time",
+      movie: "Angry Birds 2",
+      game: "Final Fantasy XIV",
+      superHero: "Tony Stark",
+      randomFact: "I like cats.",
+    }, {
+      firstname: "Kaicong",
+      lastname: "Tang",
+      color: "black",
+      animal: "dog",
+      movie: "iron man123",
+      game: "csgo",
+      superHero: "iron man",
+      randomFact: "love gaming"
+    }, {
+      firstName: "Sumedh",
+      lastName: "Vedanthi",
+      color:"Blue" ,
+      animal: "Crow",
+      book: "Of Mice and Men".
+      movie: "American Psycho",
+      game: "Fruit Ninja" ,
+      superHero: "Batman" ,
+      randomFact: "im a black belt",
+    }, {
+      firstName: "Chris",
+      lastName: "Weber",
+      color: "green",
+      animal: "cat",
+      book: "Hitchhiker's Guide to the Galaxy",
+      movie: "Spiderman Into The Spiderverse",
+      game: "The Legend of Zelda Breath of the Wild",
+      superHero: "Iron Man",
+      randomFact: "I'm vegetarian"
+    }, {
+      firstName: "Andy",
+      lastName: "Wu",
+      color: "Red",
+      animal:"Kiwi",
+      book: "Romance of the Three Kingdoms",
+      movie:"Shrek 2",
+      game: "Super Mario 64",
+      superHero: "Static Shock",
+      randomFact:"I've had jaw correction surgery"
+    }, {
+      firstName: "xiaowei",
+      lastName: "zheng",
+      color: "green",
+      animal: "babies",
+      book: "the little girl at the window",
+      movie: "eat drink man woman",
+      game: "none",
+      superHero: "none",
+      randomFact: "don't drink coke"
+    },{
+      firstName: "Vanessa",
+      lastName: "Blacow-Berggen",
+      color: "pink",
+      animal:"cat",
+      book:"The Heartstriker series",
+      superHero:"Spiderman",
+      randomFact:"I really like cats, especially my cat",
+      game: "Spyro Reignited Trilogy",
+      movie: "Cats Don't Dance"
+    } 
+
+]
