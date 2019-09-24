0299f457ff9947005385cfc022a11e8292e95aa2
Chris Weber <chris.weber260@gmail.com>
2019-09-24 16:37:46 -0700

    Added rest of student objects to array and fixed some errors

--- randomsketch/sketch.js
+++ randomsketch/sketch.js
@@ -16,7 +16,7 @@ var roster = [{
       movie: "Amelie",
       game: "none",
       superHero: "The Hulk",
-      randomFact: "I love to eat olives."
+      randomFact: "I love to eat olives.",
     }, {
       firstName: "Camila",
       lastName: "Avina Garcia",
@@ -56,7 +56,7 @@ var roster = [{
       movie: "me before you",
       game: "No",
       superHero: "Thor",
-      randomFact: " birds are tetrapods even though they only walk on two legs",
+      randomFact: "birds are tetrapods even though they only walk on two legs",
     }, {
       firstName: "Chi",
       lastName: "Nguyen",
@@ -76,12 +76,12 @@ var roster = [{
       movie: "Kung fu Hustle",
       game: "Pokemon Crystal Version",
       superHero: "Batman",
-      randomFact: "I have two pet guinea pigs"
+      randomFact: "I have two pet guinea pigs",
     }, {
 
       firstName: "Hao",
       lastName: "Rong",
-      color: hex(#4285F4),
+      color: "hex(#4285F4)",
       animal: "otter",
       book: "A Brief History of Time",
       movie: "Angry Birds 2",
@@ -89,18 +89,19 @@ var roster = [{
       superHero: "Tony Stark",
       randomFact: "I like cats.",
     }, {
-      firstname: "Kaicong",
-      lastname: "Tang",
+      firstName: "Kaicong",
+      lastName: "Tang",
       color: "black",
       animal: "dog",
+      book: "none",
       movie: "iron man123",
       game: "csgo",
       superHero: "iron man",
-      randomFact: "love gaming"
+      randomFact: "love gaming",
     }, {
       firstName: "Sumedh",
       lastName: "Vedanthi",
-      color:"Blue" ,
+      color: "Blue",
       animal: "Crow",
       book: "Of Mice and Men".
       movie: "American Psycho",
@@ -116,17 +117,17 @@ var roster = [{
       movie: "Spiderman Into The Spiderverse",
       game: "The Legend of Zelda Breath of the Wild",
       superHero: "Iron Man",
-      randomFact: "I'm vegetarian"
+      randomFact: "I'm vegetarian",
     }, {
       firstName: "Andy",
       lastName: "Wu",
       color: "Red",
-      animal:"Kiwi",
+      animal: "Kiwi",
       book: "Romance of the Three Kingdoms",
-      movie:"Shrek 2",
+      movie: "Shrek 2",
       game: "Super Mario 64",
       superHero: "Static Shock",
-      randomFact:"I've had jaw correction surgery"
+      randomFact: "I've had jaw correction surgery",
     }, {
       firstName: "xiaowei",
       lastName: "zheng",
@@ -136,17 +137,67 @@ var roster = [{
       movie: "eat drink man woman",
       game: "none",
       superHero: "none",
-      randomFact: "don't drink coke"
-    },{
+      randomFact: "don't drink coke",
+    }, {
       firstName: "Vanessa",
       lastName: "Blacow-Berggen",
       color: "pink",
-      animal:"cat",
-      book:"The Heartstriker series",
-      superHero:"Spiderman",
-      randomFact:"I really like cats, especially my cat",
+      animal: "cat",
+      book: "The Heartstriker series",
+      movie: "Cats Don't Dance",
       game: "Spyro Reignited Trilogy",
-      movie: "Cats Don't Dance"
-    } 
+      superHero: "Spiderman",
+      randomFact: "I really like cats, especially my cat",
+    }, {
+      firstName: "Matteo",
+      lastName: "Leva",
+      color: "Black",
+      animal: "Cat",
+      book: "Pillars of the earth",
+      movie: "The Godfather",
+      game: "Witcher 3",
+      superHero: "spider man",
+      randomFact: "I'm disappointed in GOT season 8",
+    }, {
+      firstName: "Anthony",
+      lastName: "Valdovinos",
+      color: "purple",
+      animal: "eagle",
+      book: "Hatchet",
+      movie: "Titanic",
+      game: "Starfox Adventures",
+      superHero: "none",
+      randomFact: "I studied Japanese for 4 years",
+    }, {
+      firstName: "Rachel",
+      lastName: "Centinaje",
+      color: "green",
+      animal: "dogs",
+      book: "none",
+      movie: "Drive",
+      game: "Dishonored",
+      superHero: "Deadpool",
+      randomFact: "I went camping this weekend",
+    }, {
+      firstName: "Alicia",
+      lastName: "Breinke",
+      color: "purple",
+      animal: "dog",
+      book: "The Art of Racing in the Rain",
+      movie: "Spirited Away",
+      game: "none",
+      superHero: "Wonder Woman",
+      randomFact: "I want to live in another country someday",
+    }, {
+      firstName: "Asra",
+      lastName: "Siddiqui",
+      color: "idk",
+      animal: "chameleons",
+      book: "codex 1962",
+      movie: "Pulp Fiction",
+      game: "idk",
+      superHero: "hate em all",
+      randomFact: "im grumpy",
+    }
 
 ]
